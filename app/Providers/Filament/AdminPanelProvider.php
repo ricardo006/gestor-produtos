941128f9482452b7a6cc0527ec79dcc0d53a6c4b
Ricardo <rycardo.191@gmail.com>
2025-02-03 00:13:54 -0300

    Ajustes Provider e config

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -39,8 +39,8 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
-                ProductSearch::class,
+                // Widgets\FilamentInfoWidget::class,
+                // ProductSearch::class,
             ])
             ->middleware([
                 EncryptCookies::class,
